be29bb701cc0a0a21b44dbf358bdac1d667834ca
kaylee42 <kayleeknowles42@gmail.com>
2016-01-03 15:17:40 -0500

    users can rate from book page

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,5 +1,11 @@
 class BookController < ApplicationController
 
+
+post '/book/rate' do
+  Rating.create(params[:rating])
+  redirect to "book/#{params[:rating][:book_id]}"
+end
+
 get '/book/:id' do
   @book = Book.find(params[:id])
   erb :"book/view"
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,7 @@
 class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+
+  validates_presence_of :amount, :user_id, :book_id
+
 end
