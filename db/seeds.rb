0bd8f2c70537543b8d1a4f5129115d2ce2f046dc
kaylee42 <kayleeknowles42@gmail.com>
2016-01-03 14:43:15 -0500

    adds overall averages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 books = Book.all.map {|g| g.id}
 users = User.all.map {|a| a.id}
-amount = [1,2,3,4,5,6,7,8,9,10]
+amount = [1,2,3,4,5,6,7,8]
 
-500.times {Rating.create(amount: amount.sample, book_id: books.sample, user_id: users.sample)}
+1000.times {Rating.create(amount: amount.sample, book_id: books.sample, user_id: users.sample)}
